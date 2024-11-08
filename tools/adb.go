3b865a20961537176b07898cc42727e29c990d4d
hezhongxian <jhonhe7night@outlook.com>
2024-11-08 21:15:45 +0800

    refactor - add adb

--- tools/adb.go
+++ tools/adb.go
@@ -27,3 +27,14 @@ func GetConnectedDevices() []string {
 	}
 	return devices
 }
+
+func CopyFilesToLocal(deviceID, remotePath, localPath string) error {
+	cmd := exec.Command("adb", "-s", deviceID, "pull", remotePath, localPath)
+	println("执行命令：", cmd)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("错误: %s\n", output)
+		return fmt.Errorf("%w", err)
+	}
+	return nil
+}
